671cdf44574f7bd8201f70f830b19c2cdafd1f8b
zjb-cmd <zjb@digitalrifle.com>
2026-01-23 14:34:55 +0000

    Add instructionParser method

--- layers/input/instructionParser.js
+++ layers/input/instructionParser.js
@@ -1,1 +1,20 @@
-export default class InstructionParser {}
+import { Instructions } from "../types/instructions.js";
+
+class InstructionParser {
+  parseInstruction(rawInstruction) {
+    const arr = [];
+    rawInstruction.split("").forEach((char) => {
+      const notSpaceChar = char !== " ";
+      const includedInTypes = Object.values(Instructions).includes(
+        char.toUpperCase(),
+      );
+      const normaliseChar = char.toUpperCase();
+
+      if (notSpaceChar && includedInTypes) arr.push(normaliseChar);
+    });
+
+    return arr;
+  }
+}
+
+export default InstructionParser;
