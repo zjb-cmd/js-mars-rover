bd9ae4317b131bc5cf0b214deb63dcef45309560
zjb-cmd <zjb@digitalrifle.com>
2026-01-23 15:23:03 +0000

    Complete plateParser method to satify test requirements

--- layers/input/plateauParser.js
+++ layers/input/plateauParser.js
@@ -1,1 +1,14 @@
-export default class PlateauParser {}
+export default class PlateauParser {
+  parsePlateauSize(inputStr) {
+    const arr = inputStr.match(/[\d]{1,3}/gm).slice(0, 2); // Get only the first two numbers, up to 3 digits each
+    const numberifiedArr = arr.map((coor) => parseInt(coor));
+
+    return numberifiedArr.length === 2 ? numberifiedArr : [];
+  }
+
+  parseCompassDirections(inputStr) {
+    return [];
+  }
+}
+
+const inst = new PlateauParser();
